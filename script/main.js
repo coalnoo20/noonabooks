52deaf467e5ad8546e77f9e19b9337266a1badca
ONDAL <lusci@naver.com>
2024-03-01 00:34:14 +0900

    🐛 상대/절대 경로 문제 해결 / ✨main.js 구역 정리 / styel_guide.html 추가 / proxy 서버 주소 분리

--- script/main.js
+++ script/main.js
@@ -1,29 +1,22 @@
+// * -------------
 // * 전역 설정
+// * -------------
+// todo : 변수 / 상수 네이밍 규칙 필요
+// todo : 전역 설정할 변수 목록 작성 필요
 
-// * const
+// ! API 관련 설정
+// todo : 알라딘 API url 키 종류별로 미리 준비
 
-const urlAPI = 'corsproxy.io/?http://www.aladin.co.kr/ttb/api/ItemSearch.aspx';
-
-const ttbKey = `ttblusci2359001`;
-
-let url = new URL(`https://${urlAPI}?ttbkey=${ttbKey}`);
-
-// ! 일반 상수
-
-// ! API 영역
-
-// ? 알라딘 API 원 구조 예시
+// ? 알라딘 API 원 구조 예시 (itemSearch)
 // `https://www.aladin.co.kr/ttb/api/ItemSearch.aspx?ttbkey=ttblusci2359001&Query=aladdin&QueryType=Title&MaxResults=10&start=1&SearchTarget=Book&output=js&Version=20131101`
 
-// * let
+const urlProxy = 'corsproxy.io/?'; // ! 프록시 서버 주소 (아래 urlAPI 예시와 같이 앞에 붙여 사용)
 
-// ! 일반 변수
+const urlAPI = urlProxy + 'http://www.aladin.co.kr/ttb/api/ItemSearch.aspx'; // ! 알라딘 ItemSearch 주소
 
-let bookList = [];
+const ttbKey = `ttblusci2359001`; // ! TTB key - 원종
 
-let callback = [];
-
-let data = '';
+let url = new URL(`https://${urlAPI}?ttbkey=${ttbKey}`);
 
 // ! 쿼리 영역
 
@@ -41,9 +34,21 @@ let outputStyle = 'js';
 
 let apiVersion = '20131101';
 
-// * html control
+// ! Const - 일반 상수 설정
 
-// * 함수 영역
+// ! let - 일반 변수
+
+let bookList = [];
+
+let callback = [];
+
+let data = '';
+
+// ! html control (object)
+
+// * -------------
+// * 함수 영역 -일반
+// * -------------
 
 // ! 알라딘 ItemSearch 가져오기
 
@@ -65,7 +70,14 @@ const getList = async () => {
     console.log('ddd', data);
 };
 
+// * -------------
+// * 함수 영역 - html/css/ui 관련
+// * -------------
+
+// * -------------
 // * 테스트 코드 영역
+// * -------------
+// todo : CORS 에러 관련 코드는 금요일 오후 8시 회의 전 삭제 예정 - ONDAL
 
 // ! JSONP 테스트
 
@@ -95,6 +107,9 @@ const testScript = () => {
     }
 };
 
+// * -------------
 // * 실행 영역
+// * -------------
+// ! 페이지 로드 후 실행이 필요한 기능
 
-console.log('run');
+console.log('start-update');
