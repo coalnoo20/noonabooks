92a4ca7d9fae90e0118227b12208aa48e576b56c
jihyeon <rlawlgus3954@naver.com>
2024-03-02 00:05:11 +0900

    매장안내페이지1차완료

--- script/main.js
+++ script/main.js
@@ -2,7 +2,7 @@
 
 // * const
 
-const urlAPI = 'corsproxy.io/?http://www.aladin.co.kr/ttb/api/ItemSearch.aspx';
+const urlAPI = "corsproxy.io/?http://www.aladin.co.kr/ttb/api/ItemSearch.aspx";
 
 const ttbKey = `ttblusci2359001`;
 
@@ -23,23 +23,23 @@ let bookList = [];
 
 let callback = [];
 
-let data = '';
+let data = "";
 
 // ! 쿼리 영역
 
-let query = 'aladdin';
+let query = "aladdin";
 
-let queryType = 'Title';
+let queryType = "Title";
 
 let maxResults = 10;
 
 let resultStart = 1;
 
-let SearchTarget = 'Book';
+let SearchTarget = "Book";
 
-let outputStyle = 'js';
+let outputStyle = "js";
 
-let apiVersion = '20131101';
+let apiVersion = "20131101";
 
 // * html control
 
@@ -48,53 +48,68 @@ let apiVersion = '20131101';
 // ! 알라딘 ItemSearch 가져오기
 
 const getList = async () => {
-    console.log('get list');
+  console.log("get list");
 
-    url.searchParams.set('Query', query);
-    url.searchParams.set('QueryType', queryType);
-    url.searchParams.set('MaxResult', maxResults);
-    url.searchParams.set('start', resultStart);
-    url.searchParams.set('SearchTarget', SearchTarget);
-    url.searchParams.set('output', outputStyle);
-    url.searchParams.set('Version', apiVersion);
+  url.searchParams.set("Query", query);
+  url.searchParams.set("QueryType", queryType);
+  url.searchParams.set("MaxResult", maxResults);
+  url.searchParams.set("start", resultStart);
+  url.searchParams.set("SearchTarget", SearchTarget);
+  url.searchParams.set("output", outputStyle);
+  url.searchParams.set("Version", apiVersion);
 
-    const response = await fetch(url);
+  const response = await fetch(url);
 
-    const data = await response.json();
+  const data = await response.json();
 
-    console.log('ddd', data);
+  console.log("ddd", data);
 };
+
+// * 구글 map api 영역
+
+const mapElement = document.getElementById("map");
+
+const map = new google.maps.Map(mapElement, {
+  center: { lat: 37.65816, lng: 126.7635 },
+  zoom: 15,
+});
+
+const marker = new google.maps.Marker({
+  position: { lat: 37.65816, lng: 126.7635 },
+  map: map,
+  title: "Team20 위치",
+});
 
 // * 테스트 코드 영역
 
 // ! JSONP 테스트
 
 const testJSONP = () => {
-    //         $.ajax({
-    //         url: url,
-    //         dataType: 'jsonp',
-    //         jsonpCallback: "myCallback",
-    //         success: callback
-    //       });
+  //         $.ajax({
+  //         url: url,
+  //         dataType: 'jsonp',
+  //         jsonpCallback: "myCallback",
+  //         success: callback
+  //       });
 
-    //       console.log(myCallback);
+  //       console.log(myCallback);
 
-    $.getJSON(url + '?callback=?', data, callback);
+  $.getJSON(url + "?callback=?", data, callback);
 };
 
 //       $.getJSON(url + "?callback=?", data, callback);
 
 const testScript = () => {
-    var script = document.createElement('script');
-    script.src = `https://www.aladin.co.kr/ttb/api/ItemSearch.aspx?ttbkey=ttblusci2359001&Query=aladdin&QueryType=Title&MaxResults=10&start=1&SearchTarget=Book&output=js&Version=20131101`;
+  var script = document.createElement("script");
+  script.src = `https://www.aladin.co.kr/ttb/api/ItemSearch.aspx?ttbkey=ttblusci2359001&Query=aladdin&QueryType=Title&MaxResults=10&start=1&SearchTarget=Book&output=js&Version=20131101`;
 
-    document.getElementsByTagName('head')[0].appendChild(script);
+  document.getElementsByTagName("head")[0].appendChild(script);
 
-    function myCallback(data) {
-        //callback method
-    }
+  function myCallback(data) {
+    //callback method
+  }
 };
 
 // * 실행 영역
 
-console.log('run');
+console.log("run");
